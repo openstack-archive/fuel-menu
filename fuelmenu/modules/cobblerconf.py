openstack-archive/fuel-web

clean up error text, bad errors in logs
Change-Id: I5b4e5cd986d9312982c80373b49199fbb2ba4363

fuelmenu/fuelmenu/modules/cobblerconf.py
@@ -269,8 +269,8 @@ interface first.")
                                   "network.")
 
         if len(errors) > 0:
-            self.parent.footer.set_text("Errors: %s First error: %s"
-                                        % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error %s" % (errors[0]))
+            log.warning("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/fuelmenu/modules/dnsandhostname.py
@@ -206,8 +206,8 @@ class dnsandhostname(urwid.WidgetWrap):
                               % responses["DNS_UPSTREAM"])
 
         if len(errors) > 0:
-            self.parent.footer.set_text(
-                "Errors: %s First error: %s" % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.warning("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/fuelmenu/modules/interfaces.py
@@ -41,7 +41,7 @@ DEFAULTS = \
         "ifname": {"label":   "Interface name:",
                    "tooltip": "Interface system identifier",
                    "value":   "locked"},
-        "onboot": {"label":   "Enabled on boot:",
+        "onboot": {"label":   "Enable interface:",
                    "tooltip": "",
                    "value":   "radio"},
         "bootproto": {"label":   "Configuration via DHCP:",
@@ -215,8 +215,8 @@ class interfaces(urwid.WidgetWrap):
             except Exception, e:
                 errors.append(e)
         if len(errors) > 0:
-            self.parent.footer.set_text("Errors: %s First error: %s" % (
-                len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.warning("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/fuelmenu/modules/rootpw.py
@@ -80,8 +80,8 @@ class rootpw(urwid.WidgetWrap):
             errors.append("Password contains non-ASCII characters.")
 
         if len(errors) > 0:
-            self.parent.footer.set_text(
-                "Errors: %s First error: %s" % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.warning("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

openstack-archive/fuel-menu

fuelmenu/modules/cobblerconf.py
@@ -269,8 +269,8 @@ interface first.")
                                   "network.")
 
         if len(errors) > 0:
-            self.parent.footer.set_text("Errors: %s First error: %s"
-                                        % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error %s" % (errors[0]))
+            log.error("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/modules/dnsandhostname.py
@@ -215,8 +215,8 @@ class dnsandhostname(urwid.WidgetWrap):
                               % responses["DNS_UPSTREAM"])
 
         if len(errors) > 0:
-            self.parent.footer.set_text(
-                "Errors: %s First error: %s" % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.error("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/modules/interfaces.py
@@ -41,7 +41,7 @@ DEFAULTS = \
         "ifname": {"label":   "Interface name:",
                    "tooltip": "Interface system identifier",
                    "value":   "locked"},
-        "onboot": {"label":   "Enabled on boot:",
+        "onboot": {"label":   "Enable interface:",
                    "tooltip": "",
                    "value":   "radio"},
         "bootproto": {"label":   "Configuration via DHCP:",
@@ -215,8 +215,8 @@ class interfaces(urwid.WidgetWrap):
             except Exception, e:
                 errors.append(e)
         if len(errors) > 0:
-            self.parent.footer.set_text("Errors: %s First error: %s" % (
-                len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.error("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")

fuelmenu/modules/rootpw.py
@@ -80,8 +80,8 @@ class rootpw(urwid.WidgetWrap):
             errors.append("Password contains non-ASCII characters.")
 
         if len(errors) > 0:
-            self.parent.footer.set_text(
-                "Errors: %s First error: %s" % (len(errors), errors[0]))
+            self.parent.footer.set_text("Error: %s" % (errors[0]))
+            log.error("Errors: %s %s" % (len(errors), errors))
             return False
         else:
             self.parent.footer.set_text("No errors found.")