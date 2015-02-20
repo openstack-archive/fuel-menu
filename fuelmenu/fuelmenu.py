openstack-archive/fuel-web

monitord user for safe API notification creation
Added monitord user with password to astute.yaml file. This is used
by Puppet manifests to create the 'monitord' user with 'monitord' role.
This allows to safely access API's /notification resource.
Usage of Keystone admin_token is not recommended nor even possible
(Keystone doesn't treat admin_token as representing any user and so
/notifications returns 401).

Change-Id: I5b4fea9e6811c2d995f058b4a0a11025e04f33fb
Partial-Bug: #1371757

fuelmenu/fuelmenu/fuelmenu.py
@@ -386,6 +386,8 @@ def save_only(iface, settingsfile='/etc/fuel/astute.yaml'):
             "keystone/nailgun_password": pwgen.password(),
             "mcollective/user": "mcollective",
             "mcollective/password": pwgen.password(),
+            "monitord/user": "monitord",
+            "monitord/password": pwgen.password(),
             "postgres/keystone_dbname": "keystone",
             "postgres/keystone_user": "keystone",
             "postgres/keystone_password": pwgen.password(),

openstack-archive/fuel-menu

fuelmenu/fuelmenu.py
@@ -384,6 +384,8 @@ def save_only(iface, settingsfile='/etc/fuel/astute.yaml'):
             "keystone/ostf_password": pwgen.password(),
             "keystone/nailgun_user": "nailgun",
             "keystone/nailgun_password": pwgen.password(),
+            "keystone/monitord_user": "monitord",
+            "keystone/monitord_password": pwgen.password(),
             "mcollective/user": "mcollective",
             "mcollective/password": pwgen.password(),
             "postgres/keystone_dbname": "keystone",