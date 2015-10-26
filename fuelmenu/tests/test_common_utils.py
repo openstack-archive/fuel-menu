fc1ef749718ef79326a42a1ef8529f020c7cecac
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-10-26 20:42:08 +0300

    Consolidate and improve hierarchy of loading of settings
    The default settings.yaml should take lowest priority,
    then /etc/fuel/astute.yaml, then custom module-specific
    sources for settings (such as parsing resolv.conf).
    
    Implemented deep merging of dicts so that keys not present
    in astute.yaml, but are present in settings.yaml, are used.
    
    Added test for utils.py
    Added dhcp_gateway default to settings.yaml
    
    Change-Id: I77d479833132e5eedca55d8b2ffe7a1b01123e1d
    Closes-Bug: #1510156

---
+++ fuelmenu/tests/test_common_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelmenu.common import utils
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_dict_merge_simple(self):
+        a = {'a': 1}
+        b = {'b': 2}
+        data = utils.dict_merge(a, b)
+        self.assertEqual(data, {'a': 1, 'b': 2})
+
+    def test_dict_merge_bad_data(self):
+        a = {'a': 1}
+        b = None
+        c = 1
+        d = (1, 2, 3)
+        e = set(['A', 'B', 'C'])
+        self.assertRaises(TypeError, utils.dict_merge, (a, b))
+        self.assertRaises(TypeError, utils.dict_merge, (b, a))
+        self.assertRaises(TypeError, utils.dict_merge, (a, c))
+        self.assertRaises(TypeError, utils.dict_merge, (d, a))
+        self.assertRaises(TypeError, utils.dict_merge, (a, e))
+
+    def test_dict_merge_override(self):
+        a = {'a': {'c': 'val'}}
+        b = {'b': 2, 'a': 'notval'}
+        data = utils.dict_merge(a, b)
+        self.assertEqual(data, {'a': 'notval', 'b': 2})
