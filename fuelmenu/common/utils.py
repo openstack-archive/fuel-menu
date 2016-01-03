1009d851058628374afbb2f1aafb86438e5a068e
Maksim Malchuk <mmalchuk@mirantis.com>
2016-01-03 20:26:32 +0300

    Fix for consts ImportError
    The consts should be imported not from top level.
    
    Change-Id: Icaf36f4f3f335d6b7497c224bf02a87c55a7c9f2
    Closes-Bug: #1530681

--- fuelmenu/common/utils.py
+++ fuelmenu/common/utils.py
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from fuelmenu import consts
+from fuelmenu.common import consts
 
 from copy import deepcopy
 import logging
