668a04e61eeed08b75050f0a867335661e756340
Valentin Kaplov <vkaplov@mirantis.com>
2016-02-10 12:13:13 +0300

    replace module unit test coverage
    Also fixed file closing after reading.
    
    Change-Id: I8485fd35a2c7346ef2d38ffd7278fdaab6dc57ad
    Partial-Bug: #1365391

--- fuelmenu/common/replace.py
+++ fuelmenu/common/replace.py
@@ -16,7 +16,8 @@ import re
 
 
 def replaceInFile(filename, orig, new):
-    lines = open(filename).readlines()
+    with open(filename) as f:
+        lines = f.readlines()
     for lineno, line in enumerate(lines):
         lines[lineno] = re.sub(orig, new, line)
     with open(filename, 'w') as f:
---
+++ fuelmenu/tests/test_common_replace.py
@@ -0,0 +1,45 @@
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from fuelmenu.common.replace import replaceInFile
+from mock import call
+from mock import mock_open
+from mock import patch
+from unittest import TestCase
+
+
+def custom_mock_open(lines):
+    """Customizing mock open
+
+    :param lines: list of lines
+    :return: mock object
+    """
+    m_open = mock_open(read_data='\n'.join(line for line in lines))
+    m_open.return_value.readlines.return_value = lines
+    return m_open
+
+
+class TestReplace(TestCase):
+    def setUp(self):
+        super(TestReplace, self).setUp()
+        self.data = ['line1\n', 'line2\n', 'line3\n']
+
+    def test_replace(self):
+        filename = 'filename'
+        with patch('__builtin__.open', custom_mock_open(self.data)) as m_open:
+            replaceInFile(filename, 'line2\n', 'line4\n')
+            self.assertEqual(m_open.call_args_list,
+                             [call(filename), call(filename, 'w')])
+            m_open.return_value.write.assert_called_once_with(
+                'line1\nline4\nline3\n')
