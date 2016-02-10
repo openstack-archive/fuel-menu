15b05e35666626bf915d989f065abc90bcaf723a
Valentin Kaplov <vkaplov@mirantis.com>
2016-02-10 15:12:23 +0300

    timeout module unit test coverage
    Change-Id: I5176b9adee1538c2a7d476a95e551027c8ccb444
    Partial-Bug: #1365391

---
+++ fuelmenu/tests/test_common_timeout.py
@@ -0,0 +1,41 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelmenu.common.timeout import run_with_timeout
+from mock import Mock
+from time import sleep
+from unittest import TestCase
+
+
+class TestRunWithTimeout(TestCase):
+    def test_run_with_timeout_default(self):
+        method = Mock(return_value='result')
+        self.assertEqual('result', run_with_timeout(method))
+        method.assert_called_once_with()
+
+    def test_run_with_params(self):
+        method = Mock(return_value='result')
+        self.assertEqual(
+            'result', run_with_timeout(method, ('a1',), {'k1': 'v1'}))
+        method.assert_called_once_with('a1', k1='v1')
+
+    def test_run_with_timeout_reached(self):
+        self.assertEqual(
+            'result',
+            run_with_timeout(sleep, (2,), timeout=1, default='result'))
+
+    def test_run_with_zero_timeout(self):
+        method = Mock(return_value='result')
+        self.assertEqual('result', run_with_timeout(method, timeout=0))
+        method.assert_called_once_with()
