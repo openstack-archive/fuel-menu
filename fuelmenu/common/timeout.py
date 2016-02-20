13ebd19d36e32d727adee08f00f1c8365783f4da
Dmitry Guryanov <dguryanov@mirantis.com>
2016-02-20 17:09:51 +0300

    Timeout module refactoring
    After a series of changes run_with_timeout became
    very strange. In case of timeout it doesn't raise
    TimeoutError, defined in this module, but instead
    returns default argument. Also it's impossible to
    distinguish if timeout reached or user pressed
    ctrl-C, because thread.interrupt_main raises
    KeyboardInterrupt in the main thread.
    
    So let's rewrite this run_with_timeout function, so
    it will raise either TimeoutError or KeyboardInterrupt.
    In this case default argument is not needed. Since it
    will always raise an exception in case of error.
    
    Also fix usage of this function and add unit tests
    mostly from I5176b9adee1538c2a7d476a95e551027c8ccb444
    change.
    
    Change-Id: Iebe7d765e648c25d49f322ece0975de527248a94

--- fuelmenu/common/timeout.py
+++ fuelmenu/common/timeout.py
@@ -12,9 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import thread
-from threading import Timer
-
+import signal
 
 class TimeoutError(Exception):
     pass
@@ -24,17 +22,24 @@ def handler(signum, frame):
     raise TimeoutError('Timeout error')
 
 
-def run_with_timeout(check, args=None, kwargs=None, timeout=60, default=False):
-    args = args if args else []
-    kwargs = kwargs if kwargs else dict()
+def run_with_timeout(func, args=tuple(),
+                     kwargs=None, timeout=60):
+    """Run function and raise exception if case of timeout.
+
+    Run function func and return what this function returned
+    if the execution took no longer than timeout. If timeout
+    exceeded function raises TimeoutError exception. If user
+    pressed Ctrl-C during this function, it raises
+    KeyboardInterrupt.
+    """
+    kwargs = kwargs or dict()
     if not timeout:
-        return check(*args, **kwargs)
+        return func(*args, **kwargs)
+
+    signal.signal(signal.SIGALRM, handler)
+    signal.setitimer(signal.ITIMER_REAL, timeout)
     try:
-        timeout_timer = Timer(timeout, thread.interrupt_main)
-        timeout_timer.start()
-        result = check(*args, **kwargs)
+        result = func(*args, **kwargs)
         return result
-    except KeyboardInterrupt:
-        return default
     finally:
-        timeout_timer.cancel()
+        signal.alarm(0)
