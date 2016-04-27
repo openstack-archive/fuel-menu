7d9fa8776a03460e978b8dfca7dcdd09333d72d5
Dmitry Russkikh <drusskikh@mirantis.com>
2016-04-27 15:29:07 +0300

    Hide Networking setup after installation
    Preventing users from changing network settings
    after deployment.
    
    Change-Id: I16ecf4b8a18769c1566b3e178d38331999216faf
    Closes-Bug: #1496604

--- fuelmenu/modules/interfaces.py
+++ fuelmenu/modules/interfaces.py
@@ -27,6 +27,7 @@ from fuelmenu.common.modulehelper import WidgetType
 from fuelmenu.common import network
 from fuelmenu.common import puppet
 from fuelmenu.common import replace
+from fuelmenu.common import utils
 import fuelmenu.common.urwidwrapper as widget
 
 blank = urwid.Divider()
@@ -40,6 +41,10 @@ class interfaces(urwid.WidgetWrap):
         self.name = "Network Setup"
         self.priority = 5
         self.visible = True
+        # Preventing users from changing network network settings
+        # after deployment.
+        if utils.is_post_deployment():
+            self.visible = False
         self.netsettings = dict()
         self.parent = parent
         self.screen = None
