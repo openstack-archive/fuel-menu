359a20af3b0d615e0e7d5109bc197cf57474c2bc
Fedor Zhadaev <fzhadaev@mirantis.com>
2016-05-20 06:37:10 +0000

    Revert "Add service_token_off to keystone hash"
    This reverts commit 48f880e9572c63ed9d1e02ce1cf6f2c992dc82ac.
    
    Change-Id: I2158d26573968cc54a219f8aa60d54457cccccf7
    Closes-Bug: #1583734

--- fuelmenu/modules/servicepws.py
+++ fuelmenu/modules/servicepws.py
@@ -74,11 +74,6 @@ class servicepws(urwid.WidgetWrap):
                     "tooltip": "",
                     "value": pwgen.password(),
                 },
-                "keystone/service_token_off": {
-                    "label": "Master node monitoring password",
-                    "tooltip": "",
-                    "value": True,
-                },
                 "mcollective/user": {"label": "Mcollective user",
                                      "tooltip": "",
                                      "value": "mcollective"},
